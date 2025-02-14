6306e40c871b64f2cce09268ebe73b64a43713ae
NeumannFilip <neumannfilip@icloud.com>
2025-02-14 15:08:16 +0100

    Apis and requests

---
+++ sayings.py
@@ -0,0 +1,12 @@
+def main():
+    hello("world")
+    goodbye("world")
+
+def hello(name ):
+    print(f"hello, {name}")
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+
+if __name__ == "__main__": #main when u run a file so it should only run with python sayings.py in others not
+    main()
