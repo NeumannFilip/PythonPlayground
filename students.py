8090065a392ec4036323e486ef25334fe99022cc
NeumannFilip <neumannfilip@icloud.com>
2025-03-06 15:04:22 +0100

    file in.out

--- students.py
+++ students.py
@@ -10,7 +10,7 @@
         name, house = line.rstrip().split(",")
         print(f"{name} is in {house}")"""
 
-students = []
+"""students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -23,4 +23,26 @@ def get_name(student):
     return student["name"]
 
 for student in sorted(students, key=get_name):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['house']}")"""
+
+
+"""import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")"""
+
+
+import csv
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file,fieldnames=["name", "home"])
+    writer.writerow({"name":name, "home":home})
